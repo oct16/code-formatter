b461cadb6c805d0d835d92d9dde6874f2ce6df40
FengFan <mail@fengfan.me>
2019-12-23 14:14:51 +0800

    fix: resolve rc path

--- src/packages/formatter/options.ts
+++ src/packages/formatter/options.ts
@@ -23,21 +23,23 @@ export const getPrettierOptions = (fileSuffix?: string) => {
     if (__path) {
         const argsPathNestArray = __path
             .split('/')
-            .reduce((acc, cur, i, arr) => {
-                acc.push([arr[i - 1], cur].join('/').toString())
-                return acc
-            }, [] as string[])
+            .reduce(
+                (acc, cur, i, arr) => {
+                    acc.push([arr[i - 1], cur].join('/').toString())
+                    return acc
+                },
+                ['/'] as string[]
+            )
             .reverse()
-
         for (const p of argsPathNestArray) {
-            const opt = prettier.resolveConfig.sync(path.resolve(process.cwd(), p, '.prettierrc'))
+            const opt = prettier.resolveConfig.sync(path.join(process.cwd(), p, '.prettierrc'))
             if (opt) {
                 prettierOptions = opt
                 break
             }
         }
     } else {
-        prettierOptions = prettier.resolveConfig.sync(path.resolve(process.cwd(), '.prettierrc'))
+        prettierOptions = prettier.resolveConfig.sync(path.join(process.cwd(), '.prettierrc'))
     }
     if (parser) {
         const specifiedOptions = {
